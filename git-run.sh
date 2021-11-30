ed120832df0d3ab4523bfa14595992470fd79fd9
Bryan Lee <leeteng2001@gmail.com>
2021-11-30 09:27:00 +0800

    Add pom file to build git into single executable, either run from 'Intellij menu > build > artifact > my-git' or use 'mvn build' command, produce two different file but same content

--- git-run.sh
+++ git-run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
-export JAVA_PROGRAM_ARGS=`echo "$@"`
-mvn exec:java -q -Dexec.mainClass=Git  -Dexec.args="$JAVA_PROGRAM_ARGS" -f /Users/lunafreya/Programming/JavaProjects/my-git/pom.xml
+# For local testing, add -q to suppress maven INFO log
 # Change -f location to local maven pom.xml location, this is specific to my machine only
 # Remember to set correct permission to run this file
+export JAVA_PROGRAM_ARGS=`echo "$@"`
+mvn exec:java -Dexec.mainClass=Git  -Dexec.args="$JAVA_PROGRAM_ARGS" -f /Users/lunafreya/Programming/JavaProjects/my-git/pom.xml
